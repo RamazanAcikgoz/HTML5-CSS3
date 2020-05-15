53e30e3959ea5c7e888f7129ba217e55c4cc5cca
Ramazan Acikgoz <ramazan.acikgz@gmail.com>
2020-05-15 16:19:01 +0200

    css + js

---
+++ project_Three/js/main.js
@@ -0,0 +1,21 @@
+let audio = null;
+
+
+
+
+document.addEventListener("keypress", function(event) {
+    let keysPiano = ["A", "S", "D","F","G","H","J","W",
+        "E","T","Y","U", "a", "s", "d","f","g","h","j","w","e","t","y","u"];
+    if (keysPiano.includes(event.key)) {
+        console.log("The '" + event.key + "' key is pressed.");
+        audio = new Audio(event.key +".mp3")
+        audio.play();
+    }
+    else {
+        console.log("Please press the keys shown on the keyboard")
+
+    }
+});
+
+
+
